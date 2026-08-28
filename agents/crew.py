178b13cd58bc32ea0904762ed05dbcb54996752d
eavandoorn <e.a.vandoorn@gmail.com>
2026-08-28 07:01:10 +0200

    create task initial_db_migrations

--- agents/crew.py
+++ agents/crew.py
@@ -1,12 +1,12 @@
 from crewai import Crew, Process
-from .tasks import setup_postgres, test_tooling, test_get_skills
+from .tasks import setup_postgres, test_tooling, test_get_skills, initial_db_migrations
 from .agents import architect, db_engineer, backend_dev, frontend_dev, sre_engineer
 
 
 # The Crew brings them together
 web_shop_crew = Crew(
     agents=[architect, db_engineer, backend_dev, frontend_dev, sre_engineer],
-    tasks=[setup_postgres], #[design_task, planning_task, ..., ...]
+    tasks=[initial_db_migrations], #[design_task, planning_task, ..., ...]
     process=Process.sequential # Task 1 must finish before Task 2 starts
 )
 
